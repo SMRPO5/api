6c33ec823cf29c8eb165532a5d03300ad9d56f92
Aljaž Košir <aljazkosir5@gmail.com>
2018-04-06 09:40:22 +0200

    Add projects to admin

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import *
+
+
+admin.site.register(Project)
+admin.site.register(CardType)
+admin.site.register(Card)
+admin.site.register(Lane)
+admin.site.register(Comment)
+admin.site.register(LoggedTime)
+admin.site.register(Task)
 
 # Register your models here.
