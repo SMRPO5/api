212bb3e26295e1dd7e07347532664277e65170c0
Aljaž Košir <aljazkosir5@gmail.com>
2018-04-20 18:59:38 +0200

    Migrations fix for deploy

--- projects/migrations/0019_auto_20180420_1803.py
+++ projects/migrations/0019_auto_20180420_1803.py
@@ -2,6 +2,24 @@
 
 from django.db import migrations, models
 import django.db.models.deletion
+from django.utils import timezone
+
+
+def create_board(apps, schema_editor):
+    Column = apps.get_model('projects', 'Column')
+    Card = apps.get_model('projects', 'Card')
+    Board = apps.get_model('projects', 'Board')
+    Project = apps.get_model('projects', 'Project')
+    DevGroup = apps.get_model('dev_groups', 'DevGroup')
+
+    board = Board.objects.create(name='initial', order='asdfadf')
+    project = Project.objects.create(name='Initial', buyer_name='Initial', codename='Initial',
+                                     start_date=timezone.now(), estimated_end_date=timezone.now, board=board, dev_group=DevGroup.objects.filter()[0])
+    column = Column.objects.create(name='placeholder', card_limit=15)
+    for card in Card.objects.all():
+        if card.column is None:
+            card.column = column
+            card.save()
 
 
 class Migration(migrations.Migration):
@@ -22,7 +40,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='column',
             name='lane',
-            field=models.ForeignKey(default=1, on_delete=django.db.models.deletion.CASCADE, related_name='columns', to='projects.Lane'),
+            field=models.ForeignKey(default=4, on_delete=django.db.models.deletion.CASCADE, related_name='columns', to='projects.Lane'),
             preserve_default=False,
         ),
         migrations.AddField(
