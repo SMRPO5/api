344df53710eb9fdeb5eb4c028ce061aac3f425f2
Aljaž Košir <aljazkosir5@gmail.com>
2018-04-22 12:15:51 +0200

    Fix 500 error when creating card

--- projects/signals/handlers.py
+++ projects/signals/handlers.py
@@ -9,7 +9,10 @@ def set_initial_column_on_card(sender, instance, **kwargs):
 	if kwargs.get('created', False):
 		first_column = Column.objects.filter(board__projects__in=[instance.project]).order_by('order').first()
 		instance.column = first_column
-		instance.order = first_column.cards.latest('order').order + 1
+		try:
+			instance.order = first_column.cards.latest('order').order + 1
+		except Card.DoesNotExist:
+			instance.order = 1
 		instance.save()
 
 
