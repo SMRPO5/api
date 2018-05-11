4f6390ffa042ad01c214d50518eafc151b2618c5
Aljaž Košir <aljazkosir5@gmail.com>
2018-05-11 08:06:39 +0200

    Attempt to filter roles on membership admin

--- dev_groups/admin.py
+++ dev_groups/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+
 from .models import DevGroup, Membership
+
+class RoleInline(admin.TabularInline):
+	model = Group
 
 
 class MembershipInline(admin.TabularInline):
 	model = Membership
 	extra = 2
+	inlines = (RoleInline, )
 
 
 class DevGroupAdmin(admin.ModelAdmin):
