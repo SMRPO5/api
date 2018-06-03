5070a0d375004e2c77b0ee8b691a66ff7e0f1505
Jan Zivkovic <zivkovicjan@gmail.com>
2018-06-03 12:21:42 +0200

    End_date is set

--- projects/views.py
+++ projects/views.py
@@ -232,7 +232,7 @@ class AnalyticsLeadTimeViewSet(CreateModelMixin, GenericViewSet):
 			return_data.append({
 				'id': card_id,
 				'start_date': start_date['date_created'],
-				'end_date': end_date['date_created']
+				'end_date': None if end_date is None else end_date['date_created']
 			})
 
 		return Response(return_data)
